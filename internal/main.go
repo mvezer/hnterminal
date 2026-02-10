f087c86e82fdb14cb8f5a60ce170c6bc46448ac6
Matyas Vezer <vezer.m@gmail.com>
2026-02-10 17:31:25 +0100

    clean up

--- internal/main.go
+++ internal/main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"fmt"
-	// apiclient "hnterminal/internal/apiclient"
-	config "hnterminal/internal/config"
-	// repository "hnterminal/internal/repository"
 	cli "hnterminal/internal/cli"
+	config "hnterminal/internal/config"
 )
 
 func main() {
@@ -17,23 +15,4 @@ func main() {
 		cli.Run()
 		defer cli.Close()
 	}
-	// api := apiclient.New(nil)
-	// repo := repository.New(nil)
-	// defer repo.Close()
-	// itemIds, err := api.GetTopStoryIds()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// storiesCount := 5
-	// for id := range storiesCount {
-	// 	item, err := repo.GetItem(itemIds[id])
-	// 	if err != nil {
-	// 		fmt.Println(err)
-	// 		continue
-	// 	}
-	// 	fmt.Printf("%s\n", "----------")
-	// 	fmt.Printf("%s\n", item.Title)
-	// 	fmt.Printf("%d\n", item.Score)
-	// 	// fmt.Printf("%v\n", item.Kids)
-	// }
 }
